5ece0cd9fb351d4df0ecab21bff895901f9bc1f1
EricFoxz <841958335@qq.com>
2022-04-25 23:43:04 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import {
-	createSSRApp
-} from "vue";
+import {createSSRApp} from "vue";
 import App from "./App.vue";
+
+import {Http} from "./utils/Http.js"
+
 export function createApp() {
-	const app = createSSRApp(App);
-	return {
-		app,
-	};
+  const app = createSSRApp(App);
+  app.config.globalProperties.$http = (url) => {
+    return new Http(url)
+  }
+
+  return {
+    app,
+  };
 }
---
+++ src/utils/Http.js
@@ -0,0 +1,68 @@
+export class Http {
+  option = {
+    header: {
+      'Access-Control-Allow-Origin': '*',
+      "Content-Type": "application/json;charset=utf-8"
+    }
+  };
+  task;
+
+  constructor(url) {
+    this.option.url = url;
+  }
+
+  get(data, success, fail) {
+    this.option.method = 'GET'
+    this.option.data = data
+    this.option.success = success
+    this.option.fail = fail
+    this._send()
+  }
+
+  post(data, success, fail) {
+    this.option.method = 'POST'
+    this.option.data = data
+    this.option.success = success
+    this.option.fail = fail
+    this._send()
+  }
+
+  put(data, success, fail) {
+    this.option.method = 'PUT'
+    this.option.data = data
+    this.option.success = success
+    this.option.fail = fail
+    this._send()
+  }
+
+  patch(data, success, fail) {
+    this.option.method = 'PATCH'
+    this.option.data = data
+    this.option.success = success
+    this.option.fail = fail
+    this._send()
+  }
+
+  delete(data, success, fail) {
+    this.option.method = 'DELETE'
+    this.option.data = data
+    this.option.success = success
+    this.option.fail = fail
+    this._send()
+  }
+
+  /**
+   * 发送请求
+   * @private
+   */
+  _send() {
+    this.task = uni.request(this.option)
+  }
+
+  /**
+   * 中断请求
+   */
+  abort() {
+    this.task.abort()
+  }
+}
