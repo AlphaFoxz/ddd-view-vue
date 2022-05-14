478cd3b48e9332fdd3992beae76439d5243a4c24
EricFoxz <841958335@qq.com>
2022-05-14 15:11:35 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import {createSSRApp} from "vue";
 import App from "./App.vue";
+import Particles from 'particles.vue3'
+import {InputPassword} from 'ant-design-vue'
 
 import {Http} from "./utils/Http.js"
 
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(Particles)
+  app.use(InputPassword)
   app.config.globalProperties.$http = (url) => {
     return new Http(url)
   }
